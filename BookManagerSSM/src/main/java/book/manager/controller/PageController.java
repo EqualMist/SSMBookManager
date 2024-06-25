c3a14b088afaadfbd98c8d71b9cd9b6454f8f3b5
zhangzeyu <akihachang@gmail.com>
2024-06-25 19:58:59 +0800

    记住我及后续功能学习

--- BookManagerSSM/src/main/java/book/manager/controller/PageController.java
+++ BookManagerSSM/src/main/java/book/manager/controller/PageController.java
@@ -3,8 +3,15 @@ package book.manager.controller;
 import book.manager.service.SimpleService;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -26,9 +33,11 @@ public class PageController {
 //    @PostAuthorize("hasRole('admin')")
     @RequestMapping("/index")
     public String index() {
-
-        List<String> list = simpleService.test();
-        System.out.println(list.toString());
+//        SecurityContext context = SecurityContextHolder.getContext();
+//        Authentication authentication = context.getAuthentication();
+//        User user = (User) authentication.getPrincipal();
+//        System.out.println(user.getUsername());
+//        System.out.println(user.getAuthorities());
         return "index";
     }
 
@@ -36,4 +45,15 @@ public class PageController {
     public String login() {
         return "login";
     }
+
+    @RequestMapping("/auth")
+    @ResponseBody
+    public String auth(){
+        SecurityContext context = SecurityContextHolder.getContext();  //获取SecurityContext对象（当前会话肯定是没有登陆的）
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("Test", null,
+                AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_user"));  //手动创建一个UsernamePasswordAuthenticationToken对象，也就是用户的认证信息，角色需要添加ROLE_前缀，权限直接写
+        context.setAuthentication(token);  //手动为SecurityContext设定认证信息
+        return "Login success！";
+    }
+
 }
